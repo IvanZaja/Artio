2332d0bda2c8b10d478f5c9539beb270c614485f
Ivan Zajaczkowski <sayonbra@gmail.com>
2024-04-27 17:34:15 +0200

    Feat: some styling like navbar or font family

---
+++ web/src/components/ui/NavBar/ArtioLogo.jsx
@@ -0,0 +1,12 @@
+
+function ArtioLogo() {
+  return (
+    <div>    
+        <img height="86" width="86" src="https://res.cloudinary.com/djfnazn3y/image/upload/v1714230578/Artio/hq6dbtmqcanmyt3xkjof.png" />
+    </div>
+  )
+}
+
+export default ArtioLogo;
+
+  
