730edf3d8c69f78100a7858eaf1ad9f9e335ec0d
Ivan Zajaczkowski <sayonbra@gmail.com>
2024-04-28 01:30:35 +0200

    Feat: Full responsive Navbar

--- web/src/components/ui/NavBar/navbar.jsx
+++ web/src/components/ui/NavBar/navbar.jsx
@@ -1,33 +1,43 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
 import ArtioLogo from "./ArtioLogo";
+import { useState } from "react";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <Navbar height='6rem' maxWidth='full'>
-          <NavbarBrand>
-            <ArtioLogo/>
-            <p className="font-bold text-inherit">Artio</p>
-          </NavbarBrand>
+        <Navbar onMenuOpenChange={setIsMenuOpen} height='6rem' maxWidth='full'>
+          <NavbarContent className="flex justify-between w-full">
+              <NavbarBrand>
+                <ArtioLogo/>
+                <p className="font-bold text-inherit text-3xl">Artio</p>
+              </NavbarBrand>
+              <NavbarMenuToggle
+                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                className="md:hidden"
+              />
+          </NavbarContent>
+
     
-          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarContent className="hidden md:flex gap-4" justify="center">
             <NavbarItem>
               <Link color="foreground" href="#">
-                Features
-              </Link>
-            </NavbarItem>
-            <NavbarItem isActive>
-              <Link href="#" aria-current="page" color="secondary">
-                Customers
+                Home
               </Link>
             </NavbarItem>
             <NavbarItem>
+              <Link href="#" color="foreground">
+                Packages
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
               <Link color="foreground" href="#">
-                Integrations
+                Our mission
               </Link>
             </NavbarItem>
           </NavbarContent>
     
-          <NavbarContent as="div" justify="end">
+          <NavbarContent as="div" justify="end" className="hidden md:flex">
             <Dropdown placement="bottom-end">
               <DropdownTrigger>
                 <Avatar
@@ -37,7 +47,7 @@ function NavBar() {
                   color="secondary"
                   name="Jason Hughes"
                   size="md"
-                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                  src="https://res.cloudinary.com/djfnazn3y/image/upload/v1713646708/portfolio/yofqhyokrbdakdhm8pj5.jpg"
                 />
               </DropdownTrigger>
               <DropdownMenu aria-label="Profile Actions" variant="flat">
@@ -45,18 +55,60 @@ function NavBar() {
                   <p className="font-semibold">Signed in as</p>
                   <p className="font-semibold">zoey@example.com</p>
                 </DropdownItem>
-                <DropdownItem key="settings">My Settings</DropdownItem>
-                <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                <DropdownItem key="analytics">Analytics</DropdownItem>
-                <DropdownItem key="system">System</DropdownItem>
-                <DropdownItem key="configurations">Configurations</DropdownItem>
-                <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-                <DropdownItem key="logout" color="danger">
+                <DropdownItem href="#">Profile</DropdownItem>
+                <DropdownItem href="#">Analytics</DropdownItem>
+                <DropdownItem color="danger"href="#">
                   Log Out
                 </DropdownItem>
               </DropdownMenu>
             </Dropdown>
           </NavbarContent>
+          <NavbarMenu>
+            <NavbarMenuItem className="divide-y">
+              <div className="content-start divide-y">
+                <div className="flex flex-col mb-5">
+                  <Link color="foreground" className="text-3xl mt-5" href="#">
+                    Home
+                  </Link>
+                  <Link color="foreground" className="text-3xl mt-5" href="#">
+                    Packages
+                  </Link>
+                  <Link color="foreground" className="text-3xl mt-5" href="#">
+                    Our mission
+                  </Link>
+                </div>
+                <div className="flex flex-col mb-5">
+                  <Link color="foreground" className="text-3xl mt-5" href="#">
+                    Profile
+                  </Link>
+                  <Link color="foreground" className="text-3xl mt-5" href="#">
+                    Analytics
+                  </Link>
+                  <Link color="danger" className="text-3xl mt-5" href="#">
+                    Log out
+                  </Link>
+                </div>
+              </div>
+              <div className="h-full divide-y">
+                <div className="flex gap-4 items-center mt-5">
+                  <Avatar
+                    isBordered
+                    as="button"
+                    className="transition-transform"
+                    color="secondary"
+                    name="Jason Hughes"
+                    size="md"
+                    src="https://res.cloudinary.com/djfnazn3y/image/upload/v1713646708/portfolio/yofqhyokrbdakdhm8pj5.jpg"
+                  />
+                  <div>
+                    <p className="font-semibold">Signed in as</p>
+                    <p className="font-semibold">zoey@example.com</p>
+                  </div>
+                </div>
+              </div>
+              
+            </NavbarMenuItem>
+          </NavbarMenu>
         </Navbar>
       );
 }
